c6e237ed7681b97bd407011c58f27d2b45538672
William Osendott <osendott@users.noreply.github.com>
2015-04-24 00:39:35 -0400

    Update replace-color.sh

--- replace-color.sh
+++ replace-color.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #quickie script to replace one color with another in theme files
-#will run recursive, so run from root folder of theme
+#will run recursive, so run from root folder of theme (Numix-Bluish-3.16)
 
 find . -type f -exec sed -i 's/<original-color>/<new-color>/g' {} +
 
